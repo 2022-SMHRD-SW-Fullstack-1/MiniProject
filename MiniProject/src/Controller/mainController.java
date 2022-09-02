1a8caff1c2c3cf5a26a60af3c4ffc3cfed76b62b
smhrd <smhrd@DESKTOP-DP07RPQ>
2022-09-02 17:35:27 +0900

    메인 컨트롤러 수정

--- MiniProject/src/Controller/mainController.java
+++ MiniProject/src/Controller/mainController.java
@@ -1,5 +1,27 @@
 package Controller;
 
 public class mainController {
-
+	
+	// 회원가입
+	public void Register() {
+		
+	}
+	// 로그인
+	public void Login() {
+		
+	}
+	
+	// 탈퇴
+	public void Dropout() {
+		
+	}
+	
+	// 삭제
+	
+	// 캐릭터 신규 생성
+	
+	// 캐릭터 선택
+	
+	// 캐릭터 삭제
+	
 }
