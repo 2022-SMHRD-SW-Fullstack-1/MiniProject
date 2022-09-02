83b663e812b9b0bdf9b267fcda4c7f7655da8f93
smhrd <smhrd@DESKTOP-DP07RPQ>
2022-09-02 15:58:24 +0900

    캐릭터 필드 지정 //김설아

--- MiniProject/src/Model/KakaoCharacter.java
+++ MiniProject/src/Model/KakaoCharacter.java
@@ -3,5 +3,8 @@ package Model;
 public class KakaoCharacter {
 
 	private String name;
+	private int Level;	//레벨(층수)
+	private int[] Experience = new int[2];	//경험치(인지도) [1] : 가진 경험치, [2] 레벨업할때 필요한 경험치
+	private int[] Energy = new int[2];		//에너지 [1] : 가진 에너지, [2] 에너지 총량
 	
 }
