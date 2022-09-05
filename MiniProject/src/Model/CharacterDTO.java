761e88edc2b16022b483da458812c1b04153fa97
smhrd <smhrd@DESKTOP-83MO3F1>
2022-09-05 12:35:10 +0900

    캐릭터 db생성 - 준호

--- MiniProject/src/Model/CharacterDTO.java
+++ MiniProject/src/Model/CharacterDTO.java
@@ -3,22 +3,22 @@ package Model;
 public class CharacterDTO {
 
 	//종류 레벨 에너지 경험치 업무능력 스트레스 월급
-	private String type; //종류
 	private String chanick; //캐릭터 닉네임
-	private int Level;	//레벨(층수)
+	private String type; //종류
 	private int Experience;	//경험치(인지도) [1] : 가진 경험치, [2] 레벨업할때 필요한 경험치
+	private int Level;	//레벨(층수)
 	private int Energy;		//에너지 [1] : 가진 에너지, [2] 에너지 총량
 	private int ability; //업무능력
 	private int stress; //스트레스
 	private int pay; //월급
 	
 	
-	public CharacterDTO(String type, String chanick, int Level, int Experience,
+	public CharacterDTO(String chanick, String type, int Experience, int Level, 
 			int Energy, int ability, int stress, int pay) {
-		this.type = type;
 		this.chanick = chanick;
-		this.Level = Level;
+		this.type = type;
 		this.Experience = Experience;
+		this.Level = Level;
 		this.Energy = Energy;
 		this.ability = ability;
 		this.stress = stress;
